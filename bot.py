264d63f0eb54fcb0ff7c29932b88d61fa9b6c4ec
TheGreatMcPain <jmcclain2020@protonmail.com>
2021-04-08 16:59:30 -0500

    Update to latest discord api, and update requirements.txt
    Also, format with yapf

--- bot.py
+++ bot.py
@@ -1,52 +1,63 @@
 import discord, json, asyncio, requests, os
 from discord.ext.commands import Bot
 
-
 primaryCurrency = ['BTC', 'LTC']
 
-altCoin = ['ACM','AEON', 'ARQ', 'BBS', 'BCN', 'BKC', 'BLOC', 'BSM', 'BTCP', 'CIV',
-'COAL', 'D', 'DASH', 'DERO', 'DOGE', 'ETH', 'ETN', 'ETNX', 'ETNXP', 'FBF', 'GPKR', 'GRFT',
-'INC', 'INTU', 'IRD', 'KRB', 'LNS', 'LOKI', 'LTC', 'LTHN', 'LUX', 'MSR', 'NAH', 'NBR', 'OMB',
-'PCN', 'PIVX', 'PIVX', 'PLURA', 'PURK', 'QTUM', 'QUAN', 'RTO', 'RVN', 'RYO', 'SHB', 'SLD', 'SOLACE',
-'SUMO', 'SUQA', 'TRTL', 'TUBE', 'WAE', 'WOW', "WTIP", 'XAO', 'XGS', 'XHV', 'XMC', 'XMR', 'XMV', 
-'XNV', 'XPP', 'XRN', 'XTA', 'XTL', 'XTRI', 'XUN', 'XVG', 'ZEL']
+altCoin = [
+    'ACM', 'AEON', 'ARQ', 'BBS', 'BCN', 'BKC', 'BLOC', 'BSM', 'BTCP', 'CIV',
+    'COAL', 'D', 'DASH', 'DERO', 'DOGE', 'ETH', 'ETN', 'ETNX', 'ETNXP', 'FBF',
+    'GPKR', 'GRFT', 'INC', 'INTU', 'IRD', 'KRB', 'LNS', 'LOKI', 'LTC', 'LTHN',
+    'LUX', 'MSR', 'NAH', 'NBR', 'OMB', 'PCN', 'PIVX', 'PIVX', 'PLURA', 'PURK',
+    'QTUM', 'QUAN', 'RTO', 'RVN', 'RYO', 'SHB', 'SLD', 'SOLACE', 'SUMO',
+    'SUQA', 'TRTL', 'TUBE', 'WAE', 'WOW', "WTIP", 'XAO', 'XGS', 'XHV', 'XMC',
+    'XMR', 'XMV', 'XNV', 'XPP', 'XRN', 'XTA', 'XTL', 'XTRI', 'XUN', 'XVG',
+    'ZEL'
+]
 
-client = Bot(command_prefix = "!")
+client = Bot(command_prefix="!")
+
 
 @client.event
 async def on_ready():
     print("Logged in")
 
+
 @client.command()
 #Tradeogre prices
-async def price(currency, crypto):
+async def price(ctx, currency, crypto):
 
-    input1 = str(currency.upper())
-    input2 = str(crypto.upper())
-       
-    if(input1 in primaryCurrency):
-        if(input2 in altCoin):
-            url = "https://tradeogre.com/api/v1/ticker/{}-{}".format(input1, input2)
+    input1 = str(currency).upper()
+    input2 = str(crypto).upper()
+
+    if (input1 in primaryCurrency):
+        if (input2 in altCoin):
+            url = "https://tradeogre.com/api/v1/ticker/{}-{}".format(
+                input1, input2)
             response = requests.get(url)
             data = response.json()
 
             price_data = data['price']
             high_data = data['high']
             low_data = data['low']
-                
-            finalString = "Current Price: " + str(price_data) + '\n' +"24hr High: " + str(high_data) + '\n' + "24hr Low: " + str(low_data)
 
-            embed = discord.Embed(title='{}-{}'.format(input1, input2), description=finalString, color =0x00ff00)
+            finalString = "Current Price: " + str(
+                price_data) + '\n' + "24hr High: " + str(
+                    high_data) + '\n' + "24hr Low: " + str(low_data)
 
-            await client.say(embed=embed)
+            embed = discord.Embed(title='{}-{}'.format(input1, input2),
+                                  description=finalString,
+                                  color=0x00ff00)
+
+            await ctx.send(embed=embed)
         else:
-            await client.say("Not a supported currency pair")
+            await ctx.send("Not a supported currency pair")
     else:
-        await client.say("Not a supported currency pair")
+        await ctx.send("Not a supported currency pair")
+
 
 #Personal API prices
 @client.command()
-async def sat():
+async def sat(ctx):
     url2 = "https://api.coindesk.com/v1/bpi/currentprice.json"
     response2 = requests.get(url2)
     data2 = response2.text
@@ -61,20 +72,23 @@ async def sat():
     eur_rate = parsed["EUR"]
     usd_rate = parsed["USD"]
 
-    usd_price = "$ `" +usd_rate + "`\n"
-    eur_price = "€ `" +eur_rate + "`\n"
+    usd_price = "$ `" + usd_rate + "`\n"
+    eur_price = "€ `" + eur_rate + "`\n"
 
-    btc_usd_price = "$ `" + usd_parsed_btc+ "`\n"
-    btc_eur_price = "€`" + eur_parsed_btc+ "`\n"
+    btc_usd_price = "$ `" + usd_parsed_btc + "`\n"
+    btc_eur_price = "€`" + eur_parsed_btc + "`\n"
 
-    data = "SAT/USD: "+ usd_price + "SAT/EUR: "+ eur_price + "BTC/USD: " + btc_usd_price + "BTC/EUR: " + btc_eur_price
+    data = "SAT/USD: " + usd_price + "SAT/EUR: " + eur_price + "BTC/USD: " + btc_usd_price + "BTC/EUR: " + btc_eur_price
 
-    embed = discord.Embed(title="Satoshi Price Data", description=data, color =0xff66cc)
-    await client.say(embed=embed)
+    embed = discord.Embed(title="Satoshi Price Data",
+                          description=data,
+                          color=0xff66cc)
+    await ctx.send(embed=embed)
+
 
 #Satoshi Calculator USD
 @client.command()
-async def satusd(number, number2):
+async def satusd(ctx, number, number2):
     url = "https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI"
     response = requests.get(url)
     data = response.text
@@ -84,14 +98,15 @@ async def satusd(number, number2):
 
     inputed_price = int(number)
     inputed_amount = int(number2)
-    price = inputed_amount*inputed_price
-    final_price = format(price*int_usd_rate, '.2f')
+    price = inputed_amount * inputed_price
+    final_price = format(price * int_usd_rate, '.2f')
 
-    await client.say('$'+str(final_price))
+    await ctx.send('$' + str(final_price))
+
 
 #Satoshi Calculator EUR
 @client.command()
-async def sateur(number, number2):
+async def sateur(ctx, number, number2):
     url = "https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI"
     response = requests.get(url)
     data = response.text
@@ -101,29 +116,36 @@ async def sateur(number, number2):
 
     inputed_price = int(number)
     inputed_amount = int(number2)
-    price = inputed_amount*inputed_price
-    final_price = format(price*int_eur_rate, '.2f')
-    
-    await client.say('€'+str(final_price))
+    price = inputed_amount * inputed_price
+    final_price = format(price * int_eur_rate, '.2f')
+
+    await ctx.send('€' + str(final_price))
+
 
 @client.command()
-async def donate():
-    await client.say("To donate please use this BTC address: 3CuYbCWtKdW6PqZHHF1oJjpmeX5Ddp6SrV ")
+async def donate(ctx):
+    await ctx.send(
+        "To donate please use this BTC address: 3CuYbCWtKdW6PqZHHF1oJjpmeX5Ddp6SrV "
+    )
+
 
 @client.command()
-async def api():
+async def api(ctx):
     data = "https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI" + '\n' + "https://api.coindesk.com/v1/bpi/currentprice.json" + '\n' + 'https://tradeogre.com/api/v1/markets'
 
     embed = discord.Embed(title="API's", description=data, color=0xff6666)
-    await client.say(embed=embed)
+    await ctx.send(embed=embed)
+
 
 #WORK ON THIS NEXT
 @client.command()
-async def online():
+async def online(ctx):
     response = requests.get("https://tradeogre.com/markets")
     statusCode = int(response.status_code)
     if statusCode == 200:
-        await client.say("Tradeogre is online.")
+        await ctx.send("Tradeogre is online.")
     else:
-        await client.say("Tradeogre is offline.")
+        await ctx.send("Tradeogre is offline.")
+
+
 client.run(os.environ.get('TOKEN', None))
